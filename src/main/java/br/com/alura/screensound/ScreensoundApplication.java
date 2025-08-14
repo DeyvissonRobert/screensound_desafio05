2a7ef5cfd0f4d4f081cc9ca8c6fe01f0cf434f39
DeyvissonRobert <robert.designerx@gmail.com>
2025-08-14 17:18:17 -0300

    Utilizando o Spring Web

--- src/main/java/br/com/alura/screensound/ScreensoundApplication.java
+++ src/main/java/br/com/alura/screensound/ScreensoundApplication.java
@@ -8,17 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreensoundApplication implements CommandLineRunner {
-	@Autowired
-	private ArtistaRepository repositorio;
+public class ScreensoundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreensoundApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
-	}
 }
